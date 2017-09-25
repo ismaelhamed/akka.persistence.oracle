518daaa7c8032c59eefd899a662e5580ee1c5ce1
Ismael Hamed <ismael.hamed@schneider-electric.com>
2017-09-25 10:24:44 +0200

    Added serialization specs

---
+++ src/Akka.Persistence.Oracle.Tests/Serialization/OracleJournalSerializationSpec.cs
@@ -0,0 +1,47 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TCK.Serialization;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Oracle.Tests.Serialization
+{
+    public class OracleJournalSerializationSpec : JournalSerializationSpec
+    {
+        private static Config Config => ConfigurationFactory.ParseString(@"
+            akka.persistence {
+                publish-plugin-commands = on
+                journal {
+                    plugin = ""akka.persistence.journal.oracle""
+                    oracle {
+                        event-adapters {
+                            custom-adapter = ""Akka.Persistence.TCK.Serialization.TestJournal+MyWriteAdapter, Akka.Persistence.TCK""
+                        }
+                        event-adapter-bindings = {
+                            ""Akka.Persistence.TCK.Serialization.TestJournal+MyPayload3, Akka.Persistence.TCK"" = custom-adapter
+                        }    
+                        class = ""Akka.Persistence.Oracle.Journal.OracleJournal, Akka.Persistence.Oracle""
+                        plugin-dispatcher = ""akka.actor.default-dispatcher""
+                        table-name = EVENTJOURNAL
+                        metadata-table-name = METADATA
+                        schema-name = AKKA_PERSISTENCE_TEST
+                        auto-initialize = on
+                        connection-string-name = ""TestDb""
+                    }
+                }
+            }");
+
+        public OracleJournalSerializationSpec(ITestOutputHelper output)
+            : base(Config, nameof(OracleJournalSerializationSpec), output)
+        { }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            DbUtils.Clean();
+        }
+
+        [Fact(Skip = "Sql plugin does not support EventAdapter.Manifest")]
+        public override void Journal_should_serialize_Persistent_with_EventAdapter_manifest()
+        { }
+    }
+}
---
+++ src/Akka.Persistence.Oracle.Tests/Serialization/OracleSnapshotSerializationSpec.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TCK.Serialization;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Oracle.Tests.Serialization
+{
+    public class OracleSnapshotSerializationSpec : SnapshotStoreSerializationSpec
+    {
+        private static Config Config => ConfigurationFactory.ParseString(@"
+            akka.persistence {
+                publish-plugin-commands = on
+                snapshot-store {
+                    plugin = ""akka.persistence.snapshot-store.oracle""
+                    oracle {
+                        class = ""Akka.Persistence.Oracle.Snapshot.OracleSnapshotStore, Akka.Persistence.Oracle""
+                        plugin-dispatcher = ""akka.actor.default-dispatcher""
+                        table-name = SNAPSHOTSTORE
+                        schema-name = AKKA_PERSISTENCE_TEST
+                        auto-initialize = on
+                        connection-string-name = ""TestDb""
+                    }
+                }
+            }");
+
+        public OracleSnapshotSerializationSpec(ITestOutputHelper output)
+            : base(Config, nameof(OracleSnapshotSerializationSpec), output)
+        { }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            DbUtils.Clean();
+        }
+    }
+}
